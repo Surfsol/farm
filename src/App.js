59452dfb4531ccfbac7c8fda1e96c12c1c9f40c3
Surfsol <surfsol@protonmail.com>
2022-02-25 18:52:28 -0300

    add copy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
-import {db} from './firebase-config'
+import { db } from './firebase-config';
 import { useEffect, useState } from 'react';
-import {collection, getDocs} from "firebase/firestore"
+import { collection, getDocs } from 'firebase/firestore';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function App() {
-  const [prices, setPrices] = useState()
-  const pricesCollection = collection(db, 'prices')
- 
-  useEffect(() => {
-    const getData = async() => {
-      const data = await getDocs(pricesCollection)
-      console.log({data})
-      setPrices(data.docs.map((doc) => ({...doc.data(), id:doc.id})))
-    }
+  const [stringArr, setStringArr] = useState();
+  const [copy, setCopy] = useState(false);
+  const pricesCollection = collection(db, 'prices');
 
-    getData()
-  },[])
-  console.log(prices[0])
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDocs(pricesCollection);
+      const arr = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const arrString = JSON.stringify(arr);
+      setStringArr(arrString);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      {prices && prices.map(item => {
-        return <div>comment:{item.comment}</div>
-      })}
+    <div style={{ display: 'flex', flexDirection: 'column', margin: 25 }}>
+      <button style={{ backgroundColor: 'lightblue', width: '200px', border: '1px solid black', height: '30px' }}>
+        <CopyToClipboard
+          text={stringArr}
+          onCopy={() => setCopy(true)}
+        >
+          {copy ? <p style={{margin:0, color: 'white'}}>Copied</p> : <p style={{margin:0}}>Copy to clipboard</p>}
+        </CopyToClipboard>
+      </button>
+      {stringArr}
     </div>
   );
 }
